43287cd64ad36ccc22a52fcf69a1582d504ae6c3
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2025-10-01 01:13:55 -0600

    t_render

--- src/losses.py
+++ src/losses.py
@@ -417,7 +417,7 @@ def pose_loss2(
 
     R_pred_w2c, t_pred_w2c = cam2obj_to_world2cam(R_pred, t_render)
     #R_gt_w2c,  t_gt_w2c    = cam2obj_to_world2cam(R_gt,  t_gt)
-    rgb_hat, sil_hat = renderer(R_pred_w2c, t_pred_w2c, K_use, image_size=(Hs,Ws))
+    rgb_hat, sil_hat = renderer(R_pred_w2c, t_render, K_use, image_size=(Hs,Ws))
     # ---- differentiable render (Kaolin DIB-R) ----
     #rgb_hat, sil_hat = renderer(R_pred, t_render, K_use, image_size=(Hs, Ws))
     sil_hat = sil_hat.float().clamp(0,1)  # (B,1,Hs,Ws)
