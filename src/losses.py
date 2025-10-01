4347cb52f0aa6a8e7a9fba65cf67539c8227ba3d
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2025-10-01 00:11:28 -0600

    _render_safe

--- src/losses.py
+++ src/losses.py
@@ -356,7 +356,7 @@ def pose_loss2(
     BG_use = F.interpolate(BG.float(), size=(Hs, Ws), mode='bilinear', align_corners=False).clamp(0,1) if mask_downsample>1 else BG.float()
 
     #Rr_pred_eff, tr_pred_eff = _compose_with_delta(R_pred, t_pred, RΔ, tΔ, sΔ)
-    rgb_hat, sil_hat = _render_safe(renderer, Rr_pred_eff, tr_pred_eff, K_r, (Hs, Ws), flip_v=flip)
+    rgb_hat, sil_hat = _render_safe(renderer, Rr_pred_s, tr_pred_s, K_r, (Hs, Ws), flip_v=flip)
     #rgb_hat, sil_hat = renderer(R_pred, t_pred, K, image_size=(Hs, Ws))
     sil_hat = _ensure_nchw(sil_hat).float().clamp(0,1)
     #if flip: sil_hat = torch.flip(sil_hat, [2])
