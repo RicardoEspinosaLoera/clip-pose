1d45ca18e1f322fc15c26ba50fd53ba3bae58d29
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2025-09-30 22:11:11 -0600

    rgb_hat

--- src/losses.py
+++ src/losses.py
@@ -286,7 +286,7 @@ def pose_loss2(
     )
 
     I_comp = composite(rgb_hat, BG, sil_hat)
-    gt_pose_iou(I, M, R_gt, t_gt, K)
+    gt_pose_iou(rgb_hat, M, R_gt, t_gt, K)
 
     logs = {
         'rot_rad': L_R.detach(),
