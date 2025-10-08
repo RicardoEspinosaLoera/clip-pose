0a27943e927bfdc44514c1ac38a5d499b02795f9
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2025-10-07 23:12:45 -0400

    forward

--- src/renderer.py
+++ src/renderer.py
@@ -49,9 +49,15 @@ class SoftMeshRenderer(torch.nn.Module):
         # --- Orientation correction (VTK -> Kaolin) ---
         #R_fix = torch.diag(torch.tensor([1.0, -1.0, -1.0], device=R.device, dtype=R.dtype))
         #R_fix = torch.diag(torch.tensor([1.0, -1.0, 1.0], device=R.device, dtype=R.dtype))
-        R_fix = torch.diag(torch.tensor([-1.0, -1.0, 1.0], device=R.device, dtype=R.dtype))
-        R = R_fix @ R
-        t = (R_fix @ t.T).T
+        R_z180 = torch.tensor([
+            [-1.0,  0.0,  0.0],
+            [ 0.0, -1.0,  0.0],
+            [ 0.0,  0.0,  1.0]
+        ], device=R.device, dtype=R.dtype)
+
+        R = R_z180 @ R
+        t = (R_z180 @ t.T).T
+
 
         # Scale vertices to better fit image
         scale = 0.15  # Increased from 0.1
