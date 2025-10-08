9460ee7b4640ff856f828923f4f3e8d4bc63c1d9
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2025-10-07 23:24:26 -0400

    forward

--- src/renderer.py
+++ src/renderer.py
@@ -50,9 +50,9 @@ class SoftMeshRenderer(torch.nn.Module):
         #R_fix = torch.diag(torch.tensor([1.0, -1.0, -1.0], device=R.device, dtype=R.dtype))
         #R_fix = torch.diag(torch.tensor([1.0, -1.0, 1.0], device=R.device, dtype=R.dtype))
         R_fix = torch.tensor([
-            [-1.0,  0.0,  0.0],
+            [ 1.0,  0.0,  0.0],
             [ 0.0, -1.0,  0.0],
-            [ 0.0,  0.0,  1.0]
+            [ 0.0,  0.0, -1.0]
         ], device=R.device, dtype=R.dtype)
 
         R = R_fix @ R
