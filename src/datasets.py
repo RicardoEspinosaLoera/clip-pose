6befd51a6866cff78a2124e4efde32fb449b6cca
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2025-10-22 15:29:25 -0400

    # src/datasets.py

--- src/datasets.py
+++ src/datasets.py
@@ -5,7 +5,7 @@ import torch
 from torch.utils.data import Dataset
 from .camera import kaolin_cam_to_K, world_to_camera_from_vtk, quat_wxyz_to_R
 import numpy as np
-import torch.nn.functional as F
+
 
 class TripletDataset(Dataset):
     """
