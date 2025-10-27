1ace884e593fda57b509c079c0620bdab79a7d96
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2025-10-27 17:47:47 -0400

    K

--- src/datasets.py
+++ src/datasets.py
@@ -128,7 +128,7 @@ class TripletDataset(Dataset):
             R_oc = torch.matmul(R_wc, R_ow)                        # [1,3,3]
             t_oc = torch.matmul(R_wc, t_ow) + t_wc      # [1,3]
 
-            K_use = kaolin_cam_to_K(cam, image_size=(W, H))
+            K = kaolin_cam_to_K(cam, image_size=(W, H))
             K_use = scale_K(K, (H, W), (Hs, Ws))
 
 
