d6cc8709612f3f58476d0451b60263d1d3722a39
RicardoEspinosaLoera <ricardo.espinosa.loera@gmail.com>
2025-10-26 21:48:40 -0400

    LoRALinear

--- src/models_dinov3.py
+++ src/models_dinov3.py
@@ -2,7 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import timm
-
+from src.LoRALinear import import (
+    _lora_wrap_linear, add_lora_to_vit_blocks
+)
 
 class DinoV3RegressorLoRA(nn.Module):
     def __init__(
